86b61ca8c94d2bf0593395384ec4d7030b37a4fe
Legat <zia-d@mail.ru>
2023-01-23 16:08:41 +0400

    feat: programs section

---
+++ components/Program.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../styles/pages/program.module.scss'
+
+export default function Program(
+  props: { class: string, src: string, heading: string }
+): JSX.Element {
+  return (
+    <div className={`${styles.wrapper} ${props.class}`}>
+      <div className={styles.border}>
+      </div>
+      <div className={styles.imgWrapper}>
+        <img src={props.src} alt='program' />
+      </div>
+      <h4 className={styles.heading}>{props.heading}</h4>
+    </div>
+  );
+}
--- components/Programs.tsx
+++ components/Programs.tsx
@@ -1,8 +1,36 @@
 import React from "react";
-import styles from '../styles/pages/landing.module.scss'
+import styles from '../styles/pages/programs.module.scss'
+import Program from "./Program";
 
 export default function Programs(): JSX.Element {
   return (
-    <div className={styles.landing}>Programs</div>
+    <section id='programs' className={styles.programs}>
+      <h2 className={styles.heading}>Programming technologies</h2>
+      <p className={`${styles.description} p1`}>
+        By the end, you’ll have the portfolio and interview skills you need to start your new career.
+      </p>
+      <div className={styles.wrapper}>
+        <Program
+          class={''}
+          src='./assets/img/program-01.png'
+          heading='Angular'
+        />
+        <Program
+          class={''}
+          src='./assets/img/program-02.png'
+          heading='React'
+        />
+        <Program
+          class={''}
+          src='./assets/img/program-03.png'
+          heading='Vue.js'
+        />
+        <Program
+          class={''}
+          src='./assets/img/program-04.png'
+          heading='JavaScript'
+        />
+      </div>
+    </section>
   );
 }
