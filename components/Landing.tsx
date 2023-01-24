78ce51d1509592528ee6256a81c2cf3e3b9febb3
Legat <zia-d@mail.ru>
2023-01-24 19:27:09 +0400

    feat: add stars to background

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -14,7 +14,25 @@ import Steps from './Steps';
 export default function Landing(): JSX.Element {
   return (
     <main className={styles.landing}>
-      <div className={styles.bgImg}></div>
+      <div className={styles.bgShiningImg}>
+        <img className={styles.star01} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star02} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star03} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star04} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star05} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star06} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star07} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star08} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star09} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star10} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star11} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star12} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star13} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star14} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star15} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star16} src={'./assets/svg/star.svg'} alt='star' />
+        <img className={styles.star17} src={'./assets/svg/star.svg'} alt='star' />
+      </div>
       <Header />
       <Showcase />
       <About />
